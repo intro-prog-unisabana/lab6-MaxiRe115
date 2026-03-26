31ed26ddbec4c28f149ad33230c9c42776b21cad
MaxiRe115 <danielmaxi115@gmail.com>
2026-03-26 14:23:26 -0500

    1.1

--- dict_concept.py
+++ dict_concept.py
@@ -1,1 +1,8 @@
 # Write your code here!
+def get_config():
+    dic = {"width": 480,
+           "height": 480,
+           "color_mode": "dark", 
+           "sensitivity": 0.4}
+    return dic
+print(get_config())
