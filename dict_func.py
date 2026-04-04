272f65885ff4426ea525a842cf3d3902015c1098
MaxiRe115 <danielmaxi115@gmail.com>
2026-04-04 12:37:25 -0500

    3.1

--- dict_func.py
+++ dict_func.py
@@ -1,1 +1,15 @@
 # Write your code here!
+def employee_print(employee_info):
+    copiae= employee_info.copy()
+
+    name = copiae.pop("Name", "N/A")
+    salry = copiae.pop("Salary", "N/A")
+    role = copiae.pop("Role", "N/A")
+    print(f"Name: {name}")
+    print(f"Salary: {salry}")
+    print(f"Role: {role}")
+    if copiae:
+        for key,value in copiae.items():
+            print(f"{key}: {value}")
+    else:
+        print("No other info!")
