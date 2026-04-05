03d01b81e0c8212817ea377d57dac4073ec20944
MaxiRe115 <danielmaxi115@gmail.com>
2026-04-05 16:50:17 -0500

    6.3

--- grades_manager.py
+++ grades_manager.py
@@ -3,18 +3,19 @@ def initialize_dict(student_name, student_grades):
     return {student_name: student_grades}
 def add_student(student_grades):
     nuevo ={}
-    name = input("Enter student name:").title()
+    name = input("Enter student name: ").title()
     while True:
-        mn= input("Enter subject and grade (or 'exit' to finish):").lower()
+        mn= input("Enter subject and grade (or 'exit' to finish): ").lower()
         if mn == 'exit':
             break
         nombre, nota = mn.split(",")    
         nombre = nombre.title().strip()
         nota = float(nota.strip())
         nuevo[nombre] = nota
-    student_grades[name]= nota
+    student_grades[name]= nuevo
     print(f"Student {name} successfully added to the grades management system.")
     return student_grades
-
+student_data = {}
+updated_data = add_student(student_data)
         
 
