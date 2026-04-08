25ee4753974aeb117194743c9892fb436bef742e
MaxiRe115 <danielmaxi115@gmail.com>
2026-04-07 19:08:03 -0500

    6.7

--- grades_manager.py
+++ grades_manager.py
@@ -25,8 +25,8 @@ def get_students(student_grades, keys):
         lkey= key.lower()
         if lkey in lresult:
             nombreo = lresult[lkey]
-            result = [nombreo.title()] = student_grades[nombreo]
+            result[nombreo.title()] = student_grades[nombreo]
         else:
-            print(f"{key.tittle()} not found!")
+            print(f"{key.title()} not found!")
     return result
 
